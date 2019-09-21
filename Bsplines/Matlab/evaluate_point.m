6e662e62f1274e9921a7033ef96feb86ef925edc
Gqia189 <35862483+Gqia189@users.noreply.github.com>
2019-09-21 07:03:41 +0000

    fix

--- Bsplines/Matlab/evaluate_point.m
+++ Bsplines/Matlab/evaluate_point.m
@@ -3,17 +3,15 @@ function [f] = evaluate_point(d, x, y, xgrid, ygrid, xyrange, Phi)
     minN = xyrange(2);
     [exmax, exmin, i] = find_gt(xgrid, x - minM);
     [eymax, eymin, j] = find_gt(ygrid, y - minN);
-    if (i < 1) || (i >= length(xgrid)) || (j < 1) || (j>=length(ygrid))
-        f = 0;
-        return 
-    end
     s = (x - minM - exmin)/ (exmax - exmin);
     t = (y - minN - eymin)/ (eymax - eymin);
     f = 0;
     for k = 0:d
         for l = 0:d
-            f = f + W(d, k, l, s, t) * Phi(i+k, j+l);
+            try
+                f = f + W(d, k, l, s, t) * Phi(i+k, j+l);
+            catch
+            end
         end
-        
     end
 end
